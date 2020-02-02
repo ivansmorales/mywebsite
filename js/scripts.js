7719b261287d9b9fbda12f9caa4f13beb796d709
Iván Morales <48416564+ivansmorales@users.noreply.github.com>
2020-02-01 23:46:35 -0600

    #title animation

--- js/scripts.js
+++ js/scripts.js
@@ -7,51 +7,42 @@
     var firstypos = window.pageYOffset;    
     $(window).scroll(function () {
         var currentypos = window.pageYOffset;
-        if (firstypos > currentypos) {
+        if(currentypos === 0){
+            $('.fixed-top').css("background-color", "#070707");
+        }else if (firstypos > currentypos) {
             $('.fixed-top').fadeIn();
             $('.fixed-top').css("background-color", "#000");
         }else{
             $('.fixed-top').fadeOut();
-            //$('.fixed-top').css("background-color", "#000");
         }
         firstypos = currentypos;
     });
 
     /*
-        Animation for navbar
+        Animation for webpage
     */
     $(window).on("load", function () {
-        $('#title .1').css('color', '#000');
-        $('#title .2').css('color', '#000');
-        $('#title .3').css('color', '#000');
-        $('#title .4').css('color', '#000');
-        $('#title .5').css('color', '#000');
+        
+        $("#title .1").css('visibility','visible').hide(0).delay(800).slideDown();
+        $("#title .2").css('visibility','visible').hide(0).delay(800).slideDown();
+        $("#title .3").css('visibility','visible').hide(0).delay(800).slideDown();
+        $("#title .4").css('visibility','visible').hide(0).delay(800).slideDown();
+        $("#title .5").css('visibility','visible').hide(0).delay(800).slideDown();
 
-        setTimeout(function () {
-            $('#title .1').css("color", "#BBCDE5");
-        }, 1000);
-        setTimeout(function () {
-            $('#title .2').css("color", "#BBCDE5");
-        }, 1300);
-        setTimeout(function () {
-            $('#title .3').css("color", "#BBCDE5");
-        }, 1600);
-        setTimeout(function () {
-            $('#title .4').css("color", "#BBCDE5");
-        }, 1900);
-        setTimeout(function () {
-            $('#title .5').css("color", "#BBCDE5");
-        }, 2200);
+        $('#title .principal').css('visibility','visible').hide(0).delay(1300).fadeIn();
+        $('#title .header').css('visibility','visible').hide(0).delay(1400).fadeIn();
+        $('#title .description').css('visibility','visible').hide(0).delay(1500).fadeIn();
+        $('#title .paragraph').css('visibility','visible').hide(0).delay(1600).slideDown();
 
-        /*
-        $('#title .1').fadeOut('fast').delay(1000).fadeIn('fast');
-        $('#title .2').fadeOut('fast').delay(1100).fadeIn('fast');
-        $('#title .3').fadeOut('fast').delay(1200).fadeIn('fast');
-        $('#title .4').fadeOut('fast').delay(1300).fadeIn('fast');
-        $('#title .5').fadeOut('fast').delay(1400).fadeIn('fast'); */
-        //$('.container-fluid .principal').fadeOut('fast').delay(1000).fadeIn('fast');
-	});
+        $(".container-nav").css('visibility','visible').hide(0).delay(2200).fadeIn();
 
+        $("#about .title").css('visibility','visible').hide(0).delay(2200).fadeIn();
+        $("#about .paragraph").css('visibility','visible').hide(0).delay(2400).fadeIn();
+        $("#about img").css('visibility','visible').hide(0).delay(2600).fadeIn();
+        $("#about ul").css('visibility','visible').hide(0).delay(2800).fadeIn();
+
+    });
+    
     /*
         Function to send email through a form.
     */
